da52c0c23c352770db591188ee670a509425b960
Simon Elliott <33635580+sce81@users.noreply.github.com>
2021-08-18 20:15:36 +0100

    Update outputs.tf
    removed additional nics

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,10 @@
-output "public_eni" {
-    value = aws_network_interface.public.id
-}
-output "mgmt_eni" {
-    value = aws_network_interface.management.id
-}
+
 output "private_eni" {
-    value = aws_network_interface.private.id
+    value = aws_network_interface.main.id
 }
-output "public_ip"  {
-    value = aws_eip.public.public_ip
-} 
-output "mgmt_ip"    {
-    value = aws_network_interface.management.private_ips
-}
+
 output "private_ip" {
-    value = aws_network_interface.private.private_ips
+    value = aws_network_interface.main.private_ips
 }
 output "instance_id" {
     value = aws_instance.main.id
