b98dec1718c8215a369bf4bdac5751394587f4fd
Simon Elliott <simon.elliott@hashicorp.com>
2023-10-04 20:14:59 +0100

    develop - removed outputs for private ips

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 
-output "private_eni" {
-  value = aws_network_interface.main.id
-}
-
-output "private_ip" {
-  value = aws_network_interface.main.private_ips
-}
+//output "private_eni" {
+//  value = aws_network_interface.main.id
+//}
+//
+//output "private_ip" {
+//  value = aws_network_interface.main.private_ips
+//}
 output "instance_id" {
   value = aws_instance.main.id
 }
