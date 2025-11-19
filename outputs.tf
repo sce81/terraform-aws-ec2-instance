3ea6d1b0d6f4402d94d5cdc45867b0a651540d37
Simon Elliott <simon.elliott@hashicorp.com>
2025-11-19 18:32:20 +0000

    develop - added additional outputs

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,13 @@
 //  value = aws_network_interface.main.private_ips
 //}
 
+output "role_name" {
+  value = aws_iam_role.main.name
+}
+
+output "role_arn" {
+  value = aws_iam_role.main.arn
+}
 output "instance_id" {
   value = aws_instance.main.id
 }
